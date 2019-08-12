fa01f92624e0bfe2f44d3175eb680d7ebeee18ae
Adam Grise <adamwgrise@gmail.com>
2019-08-12 16:48:52 -0500

    revised port definition

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 app.use(express.urlencoded({
