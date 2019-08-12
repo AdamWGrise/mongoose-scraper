7177b5e5aa2af804279bc64961da1878c3e7484c
Adam Grise <adamwgrise@gmail.com>
2019-08-12 16:55:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoosescraper";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true
-});
+mongoose.connect(MONGODB_URI);
 
 // ROUTES
 
